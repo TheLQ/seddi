46e0e023d148fa30239eac77ab80e612c1e36083
Leon Blakey <lord.quackstar@gmail.com>
2013-02-25 09:12:37 -0500

    First project create

---
+++ src/main/java/org/thelq/se/dbimport/Main.java
@@ -0,0 +1,13 @@
+package org.thelq.se.dbimportimport;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
