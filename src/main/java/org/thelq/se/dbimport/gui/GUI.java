e328a4ead6113a68abfcf350c68b8ce195a7587e
Leon Blakey <lord.quackstar@gmail.com>
2013-03-11 17:38:18 -0400

    More GUI changes

--- src/main/java/org/thelq/se/dbimport/gui/GUI.java
+++ src/main/java/org/thelq/se/dbimport/gui/GUI.java
@@ -13,8 +13,10 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -22,8 +24,10 @@ import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.WindowConstants;
@@ -38,7 +42,7 @@ public class GUI {
 	protected Controller controller;
 	protected JFrame frame;
 	protected JTable workerTable;
-	
+
 	public GUI(Controller passedController) {
 		this.controller = passedController;
 		frame = new JFrame();
@@ -55,7 +59,7 @@ public class GUI {
 				fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 				fc.setMultiSelectionEnabled(true);
 				fc.setDialogTitle("Select Folders/Files/Archives");
-				
+
 				if (fc.showOpenDialog(frame) != JFileChooser.APPROVE_OPTION)
 					return;
 				for (File curFile : fc.getSelectedFiles())
@@ -66,15 +70,19 @@ public class GUI {
 		frame.setJMenuBar(menuBar);
 
 		//Primary panel
-		FormLayout primaryLayout = new FormLayout("pref:grow, pref:grow", "pref:grow, pref:grow");
-		JPanel primaryPanel = new FormDebugPanel(primaryLayout);
+		FormLayout primaryLayout = new FormLayout("pref:grow, 6dlu, pref:grow", "pref, 6dlu, pref:grow");
+		DefaultFormBuilder primaryBuilder = new DefaultFormBuilder(primaryLayout)
+				.border(BorderFactory.createEmptyBorder(2, 2, 2, 2));
 
 		//DB Config panel
-		FormLayout configLayout = new FormLayout("pref:grow", "pref:grow");
+		FormLayout configLayout = new FormLayout("pref, 3dlu, pref, 10dlu, pref, 3dlu, pref, 10dlu, pref, 3dlu, pref", "pref:grow, pref:grow");
 		DefaultFormBuilder configBuilder = new DefaultFormBuilder(configLayout);
 		configBuilder.appendSeparator("Database Configuration");
-		configBuilder.append(new JButton("Wootz"));
-		primaryPanel.add(configBuilder.getPanel(), CC.xy(1, 1));
+		configBuilder.append("Presest", new JComboBox(new String[]{"MySQL", "SQlite", "MSSQL"}));
+		configBuilder.append("Username", new JTextField(10));
+		configBuilder.append("Password", new JPasswordField(10));
+		configBuilder.append("JDBC Connection", new JTextField(15), configLayout.getColumnCount() - 2);
+		primaryBuilder.add(configBuilder.getPanel(), CC.xy(1, 1));
 
 		//Options
 		FormLayout optionsLayout = new FormLayout("pref, 3dlu, left:pref:grow, 10dlu, pref, 3dlu, left:pref:grow", "");
@@ -83,26 +91,27 @@ public class GUI {
 		optionsBuilder.append("Disable Creating Tables", new JCheckBox());
 		optionsBuilder.append("Lower memory usage", new JCheckBox());
 		optionsBuilder.append("Global Table Prefix", new JTextField(7));
-		primaryPanel.add(optionsBuilder.getPanel(), CC.xy(2, 1));
+		primaryBuilder.add(optionsBuilder.getPanel(), CC.xy(3, 1));
 
 		//Locations
 		FormLayout locationsLayout = new FormLayout("pref:grow", "pref:grow");
 		DefaultFormBuilder locationsBuilder = new DefaultFormBuilder(locationsLayout)
 				.background(Color.WHITE);
-		locationsBuilder.appendSeparator("Dump Locations");
+		primaryBuilder.nextLine(2);
+		primaryBuilder.appendSeparator("Dump Locations");
 		for (int i = 0; i < 20; i++)
 			locationsBuilder.append(genList());
 		JScrollPane locationsPane = new JScrollPane(locationsBuilder.getPanel());
 		locationsPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		locationsPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		primaryPanel.add(locationsPane, CC.xyw(1, 2, 2));
+		primaryBuilder.append(locationsPane, 3);
 
 		//Display
-		frame.setContentPane(primaryPanel);
+		frame.setContentPane(primaryBuilder.getPanel());
 		frame.pack();
 		frame.setVisible(true);
 	}
-	
+
 	protected JComponent genList() {
 		FormLayout layout = new FormLayout("15dlu, pref:grow", "pref:grow, pref:grow");
 		final JPanel panel = new JPanel(layout);
@@ -128,7 +137,7 @@ public class GUI {
 		//panel.setBorder(BorderFactory.createLineBorder(Color.RED));
 		return panel;
 	}
-	
+
 	protected JComponent genJTableExample() {
 		final String[] columnNames = {"First Name",
 			"Last Name",
@@ -151,23 +160,23 @@ public class GUI {
 			public String getColumnName(int col) {
 				return columnNames[col].toString();
 			}
-			
+
 			public int getRowCount() {
 				return rowData.length;
 			}
-			
+
 			public int getColumnCount() {
 				return columnNames.length;
 			}
-			
+
 			public Object getValueAt(int row, int col) {
 				return rowData[row][col];
 			}
-			
+
 			public boolean isCellEditable(int row, int col) {
 				return true;
 			}
-			
+
 			public void setValueAt(Object value, int row, int col) {
 				rowData[row][col] = value;
 				fireTableCellUpdated(row, col);
@@ -175,24 +184,24 @@ public class GUI {
 		});
 		return exampleTable;
 	}
-	
+
 	protected JComponent genJTable() {
 		JTable workerTable = new JTable(new AbstractTableModel() {
 			String[] columns = {"Enabled", "Source", "# Processed", "Parser Status", "Database Status"};
-			
+
 			@Override
 			public String getColumnName(int column) {
 				return columns[column];
 			}
-			
+
 			public int getColumnCount() {
 				return columns.length;
 			}
-			
+
 			public int getRowCount() {
 				return controller.getParsers().size();
 			}
-			
+
 			public Object getValueAt(int rowIndex, int columnIndex) {
 				if (columnIndex == 0)
 					//Enabled status
@@ -203,7 +212,7 @@ public class GUI {
 		workerTable.setFillsViewportHeight(true);
 		return workerTable;
 	}
-	
+
 	public static void main(String[] args) {
 		new GUI(null);
 	}
