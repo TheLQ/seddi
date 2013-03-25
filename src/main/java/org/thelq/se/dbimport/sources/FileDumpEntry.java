c50a5aacaef20ee69d492d9b5a4c6425f0cab079
Leon Blakey <lord.quackstar@gmail.com>
2013-03-25 06:55:49 -0400

    Only return and close a single input stream for the FileDumpEntry

--- src/main/java/org/thelq/se/dbimport/sources/FileDumpEntry.java
+++ src/main/java/org/thelq/se/dbimport/sources/FileDumpEntry.java
@@ -3,6 +3,7 @@ package org.thelq.se.dbimport.sources;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,6 +22,7 @@ public class FileDumpEntry implements DumpEntry {
 	protected final File file;
 	protected DumpParser parser;
 	protected DatabaseWriter databaseWriter;
+	protected FileInputStream inputStream;
 
 	@Override
 	public String getLocation() {
@@ -34,8 +36,10 @@ public class FileDumpEntry implements DumpEntry {
 
 	@Override
 	public InputStream getInput() {
+		if(inputStream != null)
+			throw new RuntimeException("Already generated an input stream");
 		try {
-			return new FileInputStream(file);
+			return inputStream = new FileInputStream(file);
 		} catch (Exception e) {
 			throw new RuntimeException("Cannot get input stream from file " + file.getAbsolutePath());
 		}
@@ -43,8 +47,13 @@ public class FileDumpEntry implements DumpEntry {
 
 	@Override
 	public void close() {
-		parser.close();
-		databaseWriter.close();
+		try {
+			parser.close();
+			databaseWriter.close();
+			inputStream.close();
+		} catch (IOException ex) {
+			throw new RuntimeException("Cannot fully close File", ex);
+		}
 	}
 
 	public long getSizeBytes() {
