bb7fbe63309a56c6ea416e5fd280fe17f78f4849
Leon Blakey <lord.quackstar@gmail.com>
2013-03-25 16:09:11 -0400

    Provide a way to build metadataMap

--- src/main/java/org/thelq/se/dbimport/Controller.java
+++ src/main/java/org/thelq/se/dbimport/Controller.java
@@ -1,16 +1,19 @@
 package org.thelq.se.dbimport;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
 import javax.swing.SwingUtilities;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.hibernate.metadata.ClassMetadata;
+import org.hibernate.type.Type;
 import org.thelq.se.dbimport.gui.GUI;
 import org.thelq.se.dbimport.sources.DumpContainer;
 
@@ -25,6 +28,7 @@ public class Controller {
 	protected List<DumpContainer> dumpContainers = Collections.synchronizedList(new LinkedList());
 	@Getter
 	protected ExecutorService generalThreadPool;
+	protected Map<String, Map<String, Type>> metadataMap = new TreeMap(String.CASE_INSENSITIVE_ORDER);
 
 	public Controller(boolean createGui) {
 		//Copied from Executors.newCachedThreadPool()
@@ -38,6 +42,17 @@ public class Controller {
 					gui = new GUI(Controller.this);
 				}
 			});
+	}
+
+	public void initMetadataMap() {
+		for (Map.Entry<String, ClassMetadata> curEntry : DatabaseWriter.getSessionFactory().getAllClassMetadata().entrySet()) {
+			ClassMetadata tableDataRaw = curEntry.getValue();
+			Map<String, Type> properties = new TreeMap(String.CASE_INSENSITIVE_ORDER);
+			properties.put(tableDataRaw.getIdentifierPropertyName(), tableDataRaw.getIdentifierType());
+			for (String curPropertyName : tableDataRaw.getPropertyNames())
+				properties.put(curPropertyName, tableDataRaw.getPropertyType(curPropertyName));
+			metadataMap.put(curEntry.getKey(), properties);
+		}
 	}
 
 	public void addDumpContainer(DumpContainer container) {
