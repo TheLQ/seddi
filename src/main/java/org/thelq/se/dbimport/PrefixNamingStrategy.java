e28c07b2b5ab61833628608ff6f074b6c330c4f4
Leon Blakey <lord.quackstar@gmail.com>
2013-03-28 17:48:07 -0400

    New class PrefixNamingStrategy

---
+++ src/main/java/org/thelq/se/dbimport/PrefixNamingStrategy.java
@@ -0,0 +1,23 @@
+package org.thelq.se.dbimport;
+
+import lombok.RequiredArgsConstructor;
+import org.hibernate.cfg.ImprovedNamingStrategy;
+
+/**
+ * Add the specified prefix to all tables
+ * @author Leon
+ */
+@RequiredArgsConstructor
+public class PrefixNamingStrategy extends ImprovedNamingStrategy {
+	protected final String prefix;
+
+	@Override
+	public String classToTableName(String className) {
+		return prefix + super.classToTableName(className);
+	}
+
+	@Override
+	public String tableName(String tableName) {
+		return prefix + super.tableName(tableName);
+	}
+}
